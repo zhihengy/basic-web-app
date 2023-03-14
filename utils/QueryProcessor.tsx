262d1e244872b93d344e6b4fcbfd8816bfdf5064
zhihengy <zhihengy@andrew.cmu.edu>
2023-03-14 16:13:00 -0400

    new q

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -11,15 +11,20 @@ export default function QueryProcessor(query: string): string {
       "enxi15"
     );
   }
-  if (query.toLowerCase().includes("what is 21 plus 42?")) {
-    return (
-      "63"
-    );
+  if (query.toLowerCase().includes("what is a plus b?")) {
+    const query = "What is 54 multiplied by 39?";
+    const regex = /(\d+)\s*multip(?:lied\s*by\s*|plied\s*by\s*)(\d+)/i;
+    const match = query.match(regex);
+
+    if (match) {
+      const num1 = parseInt(match[1]);
+      const num2 = parseInt(match[2]);
+      const result = num1 * num2;
+      console.log(result); // 2106
+    } else {
+      console.log("Unable to extract numbers from query");
+    }
   }
-  if (query.toLowerCase().includes("what is 85 plus 46?")) {
-    return (
-      "131"
-    );
-  }
+ 
   return "";
 }
