3b4a167e8e694560ed2e29e6d0ffe82585244089
zhihengy <zhihengy@andrew.cmu.edu>
2023-03-14 15:51:34 -0400

    a

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -6,7 +6,7 @@ export default function QueryProcessor(query: string): string {
       "writer in the English language and the world's pre-eminent dramatist."
     );
   }
-  if (query.toLowerCase().includes("name")) {
+  if (query.toLowerCase().includes("what is your name?")) {
     return (
       "enxi15"
     );
